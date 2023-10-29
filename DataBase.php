4b109976af42914ab9916a53e4bd02bdb84c1b05
benbasse <moussabasse221@gmail.com>
2023-10-29 12:44:35 +0000

    Classe DataBase qui a une fonction connect() qui permet de se connecter à la base de donnée

--- DataBase.php
+++ DataBase.php
@@ -1,21 +1,6 @@
 <?php
 ini_set('display_errors', 'On');
-// $servername = "localhost";
-// $username = "ben221";
-// $password = "B@sse@bdou221";
-// $dbname = "taxi_bokko";
 
-// try 
-// {
-//     $conn = new PDO("mysql:host=$servername;dbname=taxi_bokko", $username, $password);
-// // set the PDO error mode to exception
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     echo "Connected successfully";
-// } catch(PDOException $e) 
-// {
-//     echo "Connection failed: " . $e->getMessage();
-// }
-// La classe database Pour faire la connexion, l'insertion et récupération des données
 class DataBase 
 {
     private $servername;
@@ -28,7 +13,6 @@ class DataBase
         $this->username = "ben221";
         $this->password = "B@sse@bdou221";
         $this->dbname = "nom_de_la_base";
-        
         try 
         {
             $dsn = "mysql:host=".$this->servername.";dbname=".$this->dbname;
@@ -42,20 +26,4 @@ class DataBase
     }
 
 }
-// $sername = "mysql:host:localhost;dbname=nom_de_la_base";
-// $username = "ben221";
-// $password = "B@sse@bdou221";
-// $conn = new DataBase($sername, $username, $password);
-    // public function Insert(User $user, $conn){
-    //     $sql = $conn->prepare( "INSERT INTO users (prenom, nom, telephone, email) VALUES (?, ?, ?, ?)");
-    //     $sql->execute([
-    //         $user->getPrenom(),
-    //         $user->getNom(),
-    //         $user->getTelephone(),
-    //         $user->getEmail()
-    //     ]);
-    // }
 ?>
-
-
-<!-- base->conn -->
